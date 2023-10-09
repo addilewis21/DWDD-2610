02c2020ebde58178df4f58a7ac6022bfdd0aeff8
addilewis21 <addilewis21@gmail.com>
2023-10-09 13:49:17 -0600

    adced auto

--- Auto Dealer/js/scripts.js
+++ Auto Dealer/js/scripts.js
@@ -11,3 +11,35 @@ function toggleMenu() {
 const x = document.getElementById("hamburgerBtn");
 
 x.onclick = toggleMenu;
+
+//get reviews data
+import {reviews} from '../media/reviews.js';
+console.log(reviews);
+
+const myTarget = document.querySelector("#cards");
+
+for (let x = 0; x < reviews.lenth; x++ ) {
+
+  //create a section
+  const mySection = document.createElement('section')
+  const myName = document.createElement('h3')
+  myName.textContext = reviews [x].name
+
+  const myRating = document.createElement('div')
+
+  const genIndicator = document.createElement('img')
+  genIndicator.src = "../images/star-solid.svg"
+  myRating.appendChild(genIndicator)
+
+  
+  const myReview = document.createElement('p')
+  myReview.textContent = reviews [x].text
+
+  mySection.appendChild(myName);
+
+  mySection.appendChild(myReview);
+
+  mySection.appendChild(myRating);
+
+  myTarget.appendChild(mySection);
+}
