f8d78ff447283f70459347336a5af62db82c395c
addilewis21 <addilewis21@gmail.com>
2023-11-24 15:08:49 -0700

    just gonna try this

--- Auto Dealer/js/scripts.js
+++ Auto Dealer/js/scripts.js
@@ -24,31 +24,27 @@ const myTarget = document.querySelector("#cards");
 for (let x = 0; x < reviews.length; x++ ) {
 
   //create a section
-  const mySection = document.createElement('section')
-  const myName = document.createElement('h3')
+  const mySection = document.createElement('section');
+  const myName = document.createElement('h3');
   
   //add content to h3
-  myName.textContent = reviews[x].name
-  
-  const myRating = document.createElement('div')
-// Create stars that align with the rating
-for (let step = 0; step < reviews[x].stars; step++) {
-  let img = document.createElement('img'); 
-  img.src = '../images/star.png';
-  img.alt = 'star';
-  myRating.appendChild(img);
-}
+  myName.textContent = reviews[x].name;
 
+  const myRating = document.createElement('div');
+  // Create stars that align with the rating
+  for (let step = 0; step < reviews[x].stars; step++) {
+    let img = document.createElement('img'); 
+    //add image source from images folder
+    img.src = '../images/star.png';
+    img.alt = 'star';
+    myRating.appendChild(img);
+  }
 
-
-  
-  const myReview = document.createElement('p')
-  myReview.textContent = reviews [x].text
+  const myReview = document.createElement('p');
+  myReview.textContent = reviews[x].text;
 
   mySection.appendChild(myName);
-
   mySection.appendChild(myReview);
-
   mySection.appendChild(myRating);
 
   myTarget.appendChild(mySection);
