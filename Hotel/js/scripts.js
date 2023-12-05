bf2f655b77aacf6d4be84e663d4bdf59251075a7
addilewis21 <addilewis21@gmail.com>
2023-12-04 17:31:16 -0700

    fixed phone number

--- Hotel/js/scripts.js
+++ Hotel/js/scripts.js
@@ -1,3 +1,10 @@
 // inject current year in footer
 const rightNow = new Date();
 document.querySelector('#year').textContent = rightNow.getFullYear();
+
+const hb = document.querySelector('#hamburgerBtn');
+const pw = document.querySelector('#pageWrapper');
+
+hb.addEventListener('click', () => {
+  pw.classList.toggle('moveOver');
+});
