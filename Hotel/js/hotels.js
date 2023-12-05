dff1625a12c824887ac9f1a56ad9b2c5daf350f7
addilewis21 <addilewis21@gmail.com>
2023-12-05 10:52:49 -0700

    made it?

---
+++ Hotel/js/hotels.js
@@ -0,0 +1,57 @@
+//create hotels const
+export const hotels = [
+    {
+      "name": "Hotel Lutetia",
+      "photo": "images/h1s.webp",
+      "address": ["45 Bd Raspail", "Paris, France"],
+      "phone": "+33 1 49 54 46 00",
+    },
+    {
+      "name": "Bulgari Tokyo",
+      "photo": "images/h2s.webp",
+      "address": ["2 Chome-2-1 Yaesu", "Tokyo, Japan"],
+      "phone": " +81 3-6262-3333",
+    },
+    {
+      "name": "The Ranch at Rock Creek",
+      "photo": "images/h3s.webp",
+      "address": ["79 Carriage House Ln", "Philipsburg, MT"],
+      "phone": "(877) 786-1545",
+    },
+    {
+      "name": "The Ritz-Carlton, Kyoto",
+      "photo": "images/h4s.webp",
+      "address": ["Hokodencho, 543 KAMOGAWA", "Kyoto, Japan"],
+      "phone": "+81 75-746-5555",
+    },
+    {
+      "name": "Velaa Private Island",
+      "photo": "images/h5s.webp",
+      "address": ["R6J5+GQW, Republic of Maldives", "Maldives"],
+      "phone": "+960 656-5000",
+    },
+    {
+      "name": "Maslina Resort",
+      "photo": "images/h6s.webp",
+      "address": ["Uvala Maslinica 11, 21460", "Stari Grad, Croatia"],
+      "phone": "+385 21 888 700",
+    },
+    {
+      "name": "Shiguchi",
+      "photo": "images/h7s.webp",
+      "address": ["78-5 Hanazono, Kutchan,", "Hokkaido, Japan"],
+      "phone": "+81 136-55-5235",
+    },
+    {
+      "name": "Velaa Private Island",
+      "photo": "images/h8s.webp",
+      "address": ["R6J5+GQW, Republic of Maldives", "Maldives"],
+      "phone": "+960 656-5000",
+    },
+    {
+      "name": "Velaa Private Island",
+      "photo": "images/h5s.webp",
+      "address": ["R6J5+GQW, Republic of Maldives", "Maldives"],
+      "phone": "+960 656-5000",
+    },
+]
--- Hotel/js/scripts.js
+++ Hotel/js/scripts.js
@@ -8,3 +8,35 @@ const pw = document.querySelector('#pageWrapper');
 hb.addEventListener('click', () => {
   pw.classList.toggle('moveOver');
 });
+
+//get hotel locations data from json file
+import { hotels } from '../js/hotels.js';
+
+console.log(hotels);
+
+const hotelCards = document.querySelector('#cards');
+
+//create hotel cards
+hotels.forEach((hotel) => {
+  const mySection = document.createElement('section');
+
+  const theImage = document.createElement('img');
+  theImage.src = `${hotel.photo}`;
+  theImage.alt = hotel.name;
+
+  const theName = document.createElement('h2');
+  theName.textContent = hotel.name;
+
+  const theAddress = document.createElement('address');
+  theAddress.innerHTML = `${hotel.address[0]}<br>${hotel.address[1]}`;
+
+  const thePhone = document.createElement('a');
+  thePhone.textContent = hotel.phone;
+  thePhone.href = `tel:${hotel.phone}`;
+
+  mySection.appendChild(theImage);
+  mySection.appendChild(theName);
+  mySection.appendChild(theAddress);
+  mySection.appendChild(thePhone);
+  hotelCards.appendChild(mySection);
+});
